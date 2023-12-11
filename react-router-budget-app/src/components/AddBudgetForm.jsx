fb2152e36347b246616e4aecc0e57e06e2b9351d
Kyleigh Poultney <kyleigh.poultney@gmail.com>
2023-12-11 15:02:08 -0500

    Adding Budget and Expense Components

--- react-router-budget-app/src/components/AddBudgetForm.jsx
+++ react-router-budget-app/src/components/AddBudgetForm.jsx
@@ -1,22 +1,42 @@
 import { CurrencyDollarIcon } from "@heroicons/react/24/solid"
-import { Form } from "react-router-dom"
+
+// react imports 
+import { useEffect, useRef } from "react";
+
+// rrd imports 
+import { Form, useFetcher } from "react-router-dom"
 
 const AddBudgetForm = () => {
+    const fetcher = useFetcher();
+    const isSubmitting = fetcher.state === "submitting";
+
+    const formRef = useRef();
+    const focusRef = useRef();
+
+    useEffect(() => {
+        if (!isSubmitting) {
+            formRef.current.reset()
+            focusRef.current.focus()
+        }
+    }, [isSubmitting])
+
     return (
         <div className="form-wrapper">
             <h2 className="h3">
                 Create Budget
             </h2>
-            <Form
+            <fetcher.Form
                 method="post"
-                className="grid-sm">
+                className="grid-sm"
+                ref={formRef} >
                 <div className="grid-xs">
                     <label htmlFor="newBudget">Budget Name</label>
                     <input type="text"
                         name="newBudget"
                         id="newBudget"
                         placeholder="e.g. Groceries"
-                        required />
+                        require
+                        ref={focusRef} />
                 </div>
                 <div className="grid-xs">
                     <label htmlFor="newBudgetAmount">Amount</label>
@@ -29,10 +49,16 @@ const AddBudgetForm = () => {
                         inputMode="decimal" />
                 </div>
                 <input type="hidden" name="_action" value="createBudget" />
-                <button type="submit" className="btn btn--dark">
-                    <span>Create Budget</span> <CurrencyDollarIcon width={20} />
+                <button type="submit" className="btn btn--dark" disabled={isSubmitting} >
+                    {
+                        isSubmitting ? <span>Creating budget...</span> : (
+                            <>
+                                <span>Create Budget</span> <CurrencyDollarIcon width={20} />
+                            </>
+                        )
+                    }
                 </button>
-            </Form>
+            </fetcher.Form>
         </div>
     )
 }
--- react-router-budget-app/src/helpers.js
+++ react-router-budget-app/src/helpers.js
@@ -1,3 +1,7 @@
+export const waait = () => new Promise(res => setTimeout(res, Math.random() * 800))
+
+
+// colors
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData("budgets")?.length ?? 0;
   return `${existingBudgetLength * 34} 65% 50%`
@@ -15,16 +19,65 @@ export const createBudget = ({
   const newItem = {
     id: crypto.randomUUID(),
     name: name,
-    createAt: Date.now(),
+    createdAt: Date.now(),
     amount: +amount,
     color: generateRandomColor()
   }
   const existingBudgets = fetchData("budgets") ?? [];
-  return localStorage.setItem("budgets", 
-  JSON.stringify([...existingBudgets, newItem]))
+  return localStorage.setItem("budgets",
+    JSON.stringify([...existingBudgets, newItem]))
+}
+
+// create expense
+export const createExpense = ({
+  name, amount, budgetId
+}) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    budgetId: budgetId
+  }
+  const existingExpenses = fetchData("expenses") ?? [];
+  return localStorage.setItem("expenses",
+    JSON.stringify([...existingExpenses, newItem]))
 }
 
 // delete item
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key)
+}
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+  const expenses = fetchData("expenses") ?? [];
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    // check if expense.id === budgetId I passed in
+    if (expense.budgetId !== budgetId) return acc
+
+    // add the current amount to my total
+    return acc += expense.amount
+  }, 0)
+  return budgetSpent;
+}
+
+
+// formatting
+export const formatDateToLocaleString = (epoch) => new Date(epoch).toLocaleDateString();
+
+// formatting percentages
+export const formatPercentage = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "percent",
+    minimumFractionDigits: 0,
+  })
+}
+
+// formatting currency
+export const formatCurrency = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "currency",
+    currency: "USD"
+  })
 }
